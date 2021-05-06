b3f7d6186c557c206fb634b2c72e8ec98e31e623
Jordan Haywood <51302288+JordanHay@users.noreply.github.com>
2021-05-06 09:22:06 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AlgorithmsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a string: ");
+            string textToCheck = Console.ReadLine();
+            Console.WriteLine(TextIsPalindrome(textToCheck));
+
+            Console.ReadLine();
+        }
+
+      private static bool TextIsPalindrome(string text)
+        {
+            char[] backwards = text.Reverse().ToArray();
+            char[] forwards = text.ToCharArray();
+
+            string backwardsText = new string(backwards);
+
+            bool output = (string.Compare(text, backwardsText) == 0);
+
+            return output;
+        }
+
+    }
+
+}
+
+
+
